de42636f107c443741f38b83f26ae8042ebe56f0
HAIX <victoria.snnk@gmail.com>
2022-04-08 03:15:43 -0700

    added t_malloc.

--- src/t_malloc.c
+++ src/t_malloc.c
@@ -1,10 +1,10 @@
 /*	; ---------------------------
-	Auhor: HEXION @Haixion (git)
-	Last update: 06/04/2022
+	Auhor: HEXION @Haixion (git), BASTOC @Winter-lab (git)
+	Last update: 08/04/2022
 	
 	@2600's project
-	Duo:	Bastos | realloc, calloc
-		Hexion | malloc, free, makefile, static & dynamic libraries
+	Duo:	Bastos | realloc, calloc, makefile
+		Hexion | malloc, free
 	; ---------------------------
 */
 
@@ -69,9 +69,22 @@ void memory_leak() {
 		head = NULL;
 		fclose(f);
 }
+/*
+void report_exec() {
+	char *msm_output = getenv("MSM_OUTPUT=fic.txt");
+	if(msm_output == NULL) {
+		printf("error! msm_output does not contain anything\n");
+	}
+	printf("Check: msm_output created fic.txt\n");
+}
+*/
 
 void	*my_malloc(size_t size) {
-	FILE *historic = fopen("historic.txt", "w");
+	FILE *f = fopen(msm_output,"a+");
+	// Writing the logs in dev/null bc msm_output doesn't exist and it segfaults otherwise
+	if(f == NULL) {
+		f = fopen("/dev/null", "a+");
+	}
 	/*	ATEXIT
 		Allows you to add a function that will be executed at 
 		the exit of the main function or at the exit of the 
@@ -93,12 +106,12 @@ void	*my_malloc(size_t size) {
 	}
 	
 	addr = mmap(NULL, size, PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANONYMOUS, -1, 0);
-	fprintf(historic, "Allocated memory for the pointer\n");
+	fprintf(f, "Allocated memory for the pointer\n");
 	
 	if(addr == MAP_FAILED)
 	{
 		printf("error during the mmap\n");
-		fprintf(historic, "Error during the malloc | MAP_FAILED");
+		fprintf(f, "Error during the malloc | MAP_FAILED");
 		exit(EXIT_FAILURE);
 	}
 	if(msm_output != NULL)
@@ -110,7 +123,7 @@ void	*my_malloc(size_t size) {
 	if (head == NULL)
 	{
 		t_chunk *new = mmap(NULL, sizeof(t_chunk), PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANONYMOUS, -1, 0);
-		fprintf(historic, "Created a new chunk as there was no linked list yet.\n");
+		fprintf(f, "Created a new chunk as there was no linked list yet.\n");
 		new->m_addr = addr;
 		new->m_size = size;
 		//new->m_occupe = 1;
@@ -125,7 +138,7 @@ void	*my_malloc(size_t size) {
 			temp = temp->m_next;
 
 		t_chunk *new = mmap(NULL, sizeof(t_chunk), PROT_READ|PROT_WRITE,MAP_PRIVATE|MAP_ANONYMOUS,-1,0);
-		fprintf(historic, "malloc for a new chunk");
+		fprintf(f, "malloc for a new chunk");
 		new->m_addr = addr;
 		new->m_size = size;
 		//new->m_occupe = 1;
@@ -134,8 +147,8 @@ void	*my_malloc(size_t size) {
 		temp->m_next = new;
 	}
 	return addr;
-	fprintf(historic, "Returned %p", addr);
-	fclose(historic);
+	fprintf(f, "Returned %p", addr);
+	fclose(f);
 }
 
 /*	------------------- DOUBLE FREE CHECK -------------------
@@ -144,32 +157,59 @@ void	*my_malloc(size_t size) {
 	which is freed already, the program will stop its exec
 	---------------------------------------------------------	*/
 void my_free(void *ptr) {
-	int error = 0;
+	/* FREE EXPLANATION ---------------------------------------------------------
+	1. When you call the free, you're searching for the pointer in the ll. If it's
+	there, you free it.
+	2. Otherwise, 
+	- either the pointer is NULL
+	- or it doesn't exist in the ll, then what's the point of freeing it ?
+	
+	We're going to check our LL to see if our pointer exist.
+	--------------------------------------------------------- */
+	
+	// defining the head of the linked list, assigning it to a temp to register
+	// the first block address and use it to move across the different blocks
 	t_chunk *temp = head;
 	
+	// if the head is non-void & we're not at the pointer(/address) we want to access
+	// we go to the next element 'til we find int
 	while(temp != NULL && temp->m_addr != ptr)
 		temp = temp->m_next;
 		
+	// if the head is null/doesn't contain anything, we return an error and we
+	// exit the program. it'll be of great help to counter the double free!
 	if (temp == NULL) {
 		printf("This pointer does not exist\n");
 		exit(EXIT_FAILURE);
 	}
 	
+	// if there are more than 2 blocks in the ll, we're going to re-define the next
+	// and the previous of the block(=the head) to delete little by little the different
+	// blocks composing the linked list.
+	// in all of these cases, we redirect the pointer to NULL to get it out of the linked list 
 	if (temp->m_next != NULL && temp->m_prev != NULL)
 	{
 		temp->m_prev->m_next = temp->m_next;
 		temp->m_next->m_prev = temp->m_prev;
 	}
+	// if there's 2 elts
 	else if (temp->m_next == NULL && temp->m_prev!=NULL)
+	// the previous point to the next but! the next is null
+	// so the previous point to the null, and the previous
+	// becomes null
 		temp->m_prev->m_next = temp->m_next;
-		
+	
+	// if there's one and only one block in the ll	
 	else if (temp->m_next != NULL && temp->m_prev==NULL)
 	{
+		// the head becomes the next, !null
+		// and the first elt of the list becomes equivalent of NULL
 		head = temp->m_next;
 		temp->m_next->m_prev = temp->m_prev;
 	}
 	else head = NULL;
 
+	/* ------------ Error messages in case of problems --------------- */
 	if (munmap(temp->m_addr,temp->m_size) != 0)
 	{
 		printf("Error on munmap");
@@ -192,3 +232,73 @@ void my_free(void *ptr) {
 				@BASTOS
 	realloc & calloc
 	----------------------------------------------------	*/
+
+void *my_calloc(size_t size_el, size_t nb_el) {
+	if (call_memory_leak == 0)     // Linked through atexit
+	{
+		call_memory_leak = 1;
+		atexit(memory_leak);    // Verify a function call in order to check memory_leak through atexit by single-use call of it.
+	}
+	
+	void *addr = NULL;
+
+	if(size_el == 0) {
+		printf("size is null\n");
+		return(NULL);
+	}
+	
+    if(nb_el == 0) {
+		printf("number of elements is null\n");
+		return(NULL);
+	}
+
+    addr = my_malloc(nb_el * size_el);
+
+    if(addr){
+
+        char *byte = addr;
+
+        while(byte != addr+size_el*nb_el){
+            //memset(&byte, 0, nb_el);
+			*byte=0;
+            printf("Value test per byte : %c\n", *byte);
+            byte++;
+        }
+
+    }
+	return(addr);
+}
+
+void *my_realloc(void *ptr, size_t size){
+    t_chunk *new_alloc = head;	// Running through the meta data linked list
+
+	if(new_alloc == NULL){
+        printf("Error: header null\n");  
+        call_memory_leak = 1;
+        return(MAP_FAILED);
+    }
+
+    while(new_alloc->m_addr!=ptr){
+
+        if(new_alloc == NULL){
+            printf("Error: block not found\n");  
+            call_memory_leak = 1;
+            return(MAP_FAILED);
+        }
+		new_alloc = new_alloc->m_next;
+
+    }
+
+    new_alloc->m_addr = mremap(new_alloc->m_addr, new_alloc->m_size, size, MREMAP_MAYMOVE);
+	
+	if(new_alloc->m_addr == MAP_FAILED){
+		printf("Error: No sufficient space found for reallocating | MAP FAILED\n");
+		call_memory_leak = 1;
+		return(MAP_FAILED);
+	}
+
+    new_alloc->m_size = size;
+    printf("Reallocated memory into the memory space: %p\n", new_alloc->m_addr);
+    return(new_alloc->m_addr);
+}
+
